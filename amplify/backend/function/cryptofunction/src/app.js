361b1d92d018a00086360145464b454f3570262d
Autumn <austinnbrown95@gmail.com>
2024-03-21 14:17:55 -0500

    got lab 5 working

--- amplify/backend/function/cryptofunction/src/app.js
+++ amplify/backend/function/cryptofunction/src/app.js
@@ -1,4 +1,3 @@
-
 /*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
@@ -25,6 +24,30 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "*")
   next()
 });
+
+/* Creating the coins route using the textbook code */
+
+//Import axios
+const axios = require('axios')
+
+app.get('/coins', function(req, res) {
+  // Define base url
+  let apiUrl = `https://api.coinlore.com/api/tickers?start=0&limit=10`
+
+  // Check if there are any query string parameters
+  // If so, reset the base url to include them
+  if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+   const { start = 0, limit = 10 } = req.apiGateway.event.queryStringParameters
+   apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`
+  }
+
+  // Call API and return response
+  axios.get(apiUrl)
+    .then(response => {
+      res.json({  coins: response.data.data })
+    })
+    .catch(err => res.json({ error: err }))
+})
 
 
 /**********************
--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+// Source: https://docs.amplify.aws/react/build-a-backend/restapi/fetch-data/
+import { get } from 'aws-amplify/api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    // Create coins variable and set to empty array
+    const [coins, updateCoins] = useState([]);
 
-export default App;
+    // Create additional state to hold user input for limit and start properties
+    const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+    // Create a variable for loading
+    const [loading, updateLoading] = useState(true);
+
+    // Create a new function to allow users to update the input values
+    function updateInputValues(type, value) {
+        updateInput({ ...input, [type]: value });
+    }
+
+    // Define function to call API
+    const fetchCoins = async() => {
+      updateLoading(true);
+      const { limit, start } = input
+      //Get request with latest Amplify
+      const restOperation = await get({
+        apiName: "cryptoapi",
+        path: "/coins",
+        options: { // https://docs.amplify.aws/react/build-a-backend/restapi/fetch-data/#accessing-query-parameters--body-in-lambda-proxy-function
+          queryParams: {
+            limit: limit,
+            start: start
+          }
+        }
+      });
+      // Source: https://docs.amplify.aws/react/build-a-backend/restapi/fetch-data/#accessing-response-payload
+      const { body } = await restOperation.response;
+      const json = await body.json();
+      updateCoins(json.coins);
+      updateLoading(false);
+    }
+
+    // Call fetchCoins function when component loads
+    useEffect(() => {
+      fetchCoins()
+    }, [])
+
+    return (
+      <div className="App">
+        <input
+            placeholder="start"
+            onChange={e => updateInputValues('start', e.target.value)}
+        />
+        <input
+            onChange={e => updateInputValues('limit', e.target.value)}
+            placeholder="limit"
+        />
+        <button onClick={fetchCoins}>Fetch Coins</button>
+        {loading && <h2>Loading...</h2>}
+         {
+           !loading && coins.map((coin, index) => (
+            <div key={index}>
+              <h2>{coin.name} - {coin.symbol}</h2>
+              <h5>${coin.price_usd}</h5>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+
+  export default App
